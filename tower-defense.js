79bbc64484600da64784e0c32f9e23f1a7f5bffd
dylanclarke890 <dylanclarke890@gmail.com>
2022-08-16 01:12:18 +0100

    grouped playable units

--- tower-defense.js
+++ tower-defense.js
@@ -7,343 +7,350 @@ canvas.height = 600;
  *              B A S E  C L A S S E S
  */
 
-class BaseCanvasModel {
-  constructor(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
-  update() {
-    throw new Error("method not implemented");
-  }
-  draw() {
-    throw new Error("method not implemented");
-  }
-}
+ class Sprite {
+   constructor(src, width, height, minFrame, maxFrame) {
+     this.image = new Image();
+     this.image.src = src;
+     this.width = width;
+     this.height = height;
+     this.minFrame = minFrame;
+     this.maxFrame = maxFrame;
+   }
+ }
 
-class BaseUnit extends BaseCanvasModel {
-  constructor(
-    type,
-    x,
-    y,
-    width,
-    height,
-    health,
-    minFrame,
-    maxFrame,
-    spriteWidth,
-    spriteHeight
-  ) {
-    super(x, y, width, height);
-    this.type = type;
-    this.health = health;
-    this.maxHealth = health;
-    this.frameX = 0;
-    this.frameY = 0;
-    this.minFrame = minFrame;
-    this.maxFrame = maxFrame;
-    this.spriteWidth = spriteWidth;
-    this.spriteHeight = spriteHeight;
-  }
+ class BaseCanvasModel {
+   constructor(x, y, width, height) {
+     this.x = x;
+     this.y = y;
+     this.width = width;
+     this.height = height;
+   }
+   update() {
+     throw new Error("method not implemented");
+   }
+   draw() {
+     throw new Error("method not implemented");
+   }
+ }
 
-  get unitHealth() {
-    return Math.floor(this.health);
-  }
+ class BaseUnit extends BaseCanvasModel {
+   constructor(x, y, width, height, health, sprite) {
+     super(x, y, width, height);
+     this.health = health;
+     this.maxHealth = health;
+     this.frameX = 0;
+     this.frameY = 0;
+     this.sprite = sprite;
+   }
 
-  nextSpriteFrame() {
-    if (this.frameX < this.maxFrame) this.frameX++;
-    else this.frameX = this.minFrame;
-  }
+   get unitHealth() {
+     return Math.floor(this.health);
+   }
 
-  drawHP(fillStyle, font, x, y) {
-    ctx.fillStyle = fillStyle;
-    ctx.font = font;
-    ctx.fillText(this.unitHealth, x, y);
-  }
+   nextSpriteFrame() {
+     if (this.frameX < this.sprite.maxFrame) this.frameX++;
+     else this.frameX = this.sprite.minFrame;
+   }
 
-  drawSprite() {
-    ctx.drawImage(
-      this.type.image,
-      this.frameX * this.spriteWidth,
-      0,
-      this.spriteWidth,
-      this.spriteHeight,
-      this.x,
-      this.y,
-      this.width,
-      this.height
-    );
-  }
-}
+   drawHP(fillStyle, font, x, y) {
+     ctx.fillStyle = fillStyle;
+     ctx.font = font;
+     ctx.fillText(this.unitHealth, x, y);
+   }
 
-/***********************************************************
- *              G L O B A L  V A R I A B L E S
- */
+   drawSprite() {
+     ctx.drawImage(
+       this.sprite.image,
+       this.frameX * this.sprite.width,
+       0,
+       this.sprite.width,
+       this.sprite.height,
+       this.x,
+       this.y,
+       this.width,
+       this.height
+     );
+   }
+ }
 
-const player = {
-  projectiles: [],
-  resources: 300,
-  score: 0,
-  selectedUnit: 0, // the index of the unit type to place next.
-  units: [],
-};
+ /***********************************************************
+  *              G L O B A L  V A R I A B L E S
+  */
 
-const enemy = {
-  frequency: 600, // how often in frames to spawn a new enemy.
-  positions: [],
-  units: [],
-};
+ const player = {
+   projectiles: [],
+   resources: 300,
+   score: 0,
+   selectedUnit: 0, // the index of the unit type to place next.
+   units: [],
+ };
 
-const gameState = {
-  frame: 0,
-  messages: [],
-  over: false,
-  pickups: [],
-  winningScore: 50,
-};
+ const enemy = {
+   frequency: 600, // how often in frames to spawn a new enemy.
+   positions: [],
+   units: [],
+ };
 
-const plant = new Image();
-plant.src = "sprites/plant.png";
-const zombie = new Image();
-zombie.src = "sprites/zombie.png";
+ const gameState = {
+   frame: 0,
+   messages: [],
+   over: false,
+   pickups: [],
+   winningScore: 50,
+ };
 
-const board = {
-  cell: {
-    gap: 3,
-    size: 100,
-  },
-  grid: [],
-  resources: {
-    amounts: [20, 30, 40],
-  },
-  units: {
-    player: [
-      {
-        x: 10,
-        y: 10,
-        width: 70,
-        height: 85,
-        image: plant,
-      },
-      {
-        x: 90,
-        y: 10,
-        width: 70,
-        height: 85,
-        image: plant,
-      },
-    ],
-    enemy: [{ image: zombie }],
-  },
-};
+ const plant = new Sprite("sprites/plant.png", 167, 256, 0, 1);
+ const zombie = new Sprite("sprites/zombie.png", 290, 420, 0, 7);
 
-const mouse = {
-  x: 10,
-  y: 10,
-  width: 0.1,
-  height: 0.1,
-  clicked: false,
-};
+ const playable = {
+   units: {
+     player: [plant, plant],
+     enemy: [zombie],
+   },
+ };
 
-const actionBar = {
-  canvasPosition: canvas.getBoundingClientRect(),
-  width: canvas.width,
-  height: board.cell.size,
-  unitStrokes: {
-    normal: "black",
-    selected: "gold",
-  },
-};
+ const board = {
+   cell: {
+     gap: 3,
+     size: 100,
+   },
+   grid: [],
+   resources: {
+     amounts: [20, 30, 40],
+   },
+ };
 
-canvas.addEventListener("mousemove", (e) => {
-  mouse.x = e.x - actionBar.canvasPosition.left;
-  mouse.y = e.y - actionBar.canvasPosition.top;
-});
-canvas.addEventListener("mouseleave", () => {
-  mouse.x = undefined;
-  mouse.y = undefined;
-});
-canvas.addEventListener("mousedown", () => {
-  mouse.clicked = true;
-});
-canvas.addEventListener("mouseup", () => {
-  mouse.clicked = false;
-});
+ const mouse = {
+   x: 10,
+   y: 10,
+   width: 0.1,
+   height: 0.1,
+   clicked: false,
+ };
 
-/***********************************************************
- *              G A M E  B O A R D
- */
+ const actionBar = {
+   canvasPosition: canvas.getBoundingClientRect(),
+   width: canvas.width,
+   height: board.cell.size,
+   unitStrokes: {
+     normal: "black",
+     selected: "gold",
+   },
+ };
 
-class Cell extends BaseCanvasModel {
-  constructor(x, y) {
-    super(x, y, board.cell.size, board.cell.size);
-  }
+ canvas.addEventListener("mousemove", (e) => {
+   mouse.x = e.x - actionBar.canvasPosition.left;
+   mouse.y = e.y - actionBar.canvasPosition.top;
+ });
+ canvas.addEventListener("mouseleave", () => {
+   mouse.x = undefined;
+   mouse.y = undefined;
+ });
+ canvas.addEventListener("mousedown", () => {
+   mouse.clicked = true;
+ });
+ canvas.addEventListener("mouseup", () => {
+   mouse.clicked = false;
+ });
 
-  draw() {
-    if (mouse.x && mouse.y && isColliding(this, mouse)) {
-      ctx.strokeStyle = "black";
-      ctx.strokeRect(this.x, this.y, this.width, this.height);
-    }
-  }
-}
+ /***********************************************************
+  *              G A M E  B O A R D
+  */
 
-(function createGrid() {
-  for (let y = board.cell.size; y < canvas.height; y += board.cell.size)
-    for (let x = 0; x < canvas.width; x += board.cell.size)
-      board.grid.push(new Cell(x, y));
-})();
+ class Cell extends BaseCanvasModel {
+   constructor(x, y) {
+     super(x, y, board.cell.size, board.cell.size);
+   }
 
-function handleGameGrid() {
-  for (let i = 0; i < board.grid.length; i++) board.grid[i].draw();
-}
+   draw() {
+     if (mouse.x && mouse.y && isColliding(this, mouse)) {
+       ctx.strokeStyle = "black";
+       ctx.strokeRect(this.x, this.y, this.width, this.height);
+     }
+   }
+ }
 
-/***********************************************************
- *              P R O J E C T I L E S
- */
+ (function createGrid() {
+   for (let y = board.cell.size; y < canvas.height; y += board.cell.size)
+     for (let x = 0; x < canvas.width; x += board.cell.size)
+       board.grid.push(new Cell(x, y));
+ })();
 
-class Projectile extends BaseCanvasModel {
-  constructor(x, y) {
-    super(x, y, 10, 10);
-    this.power = 20;
-    this.speed = 5;
-  }
+ function handleGameGrid() {
+   for (let i = 0; i < board.grid.length; i++) board.grid[i].draw();
+ }
 
-  update() {
-    this.x += this.speed;
-  }
+ /***********************************************************
+  *              P R O J E C T I L E S
+  */
 
-  draw() {
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2);
-    ctx.fill();
-  }
-}
+ class Projectile extends BaseCanvasModel {
+   constructor(x, y) {
+     super(x, y, 10, 10);
+     this.power = 20;
+     this.speed = 5;
+   }
 
-function handleProjectiles() {
-  for (let i = 0; i < player.projectiles.length; i++) {
-    player.projectiles[i].update();
-    player.projectiles[i].draw();
+   update() {
+     this.x += this.speed;
+   }
 
-    for (let j = 0; j < enemy.units.length; j++) {
-      if (
-        enemy.units[j] &&
-        player.projectiles[i] &&
-        isColliding(player.projectiles[i], enemy.units[j])
-      ) {
-        enemy.units[j].health -= player.projectiles[i].power;
-        player.projectiles.splice(i, 1);
-        i--;
-      }
-    }
+   draw() {
+     ctx.fillStyle = "black";
+     ctx.beginPath();
+     ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2);
+     ctx.fill();
+   }
+ }
 
-    if (
-      player.projectiles[i] &&
-      player.projectiles[i].x > canvas.width - board.cell.size
-    ) {
-      player.projectiles.splice(i, 1);
-      i--;
-    }
-  }
-}
+ function handleProjectiles() {
+   for (let i = 0; i < player.projectiles.length; i++) {
+     player.projectiles[i].update();
+     player.projectiles[i].draw();
 
-/***********************************************************
- *              D E F E N D E R S
- */
+     for (let j = 0; j < enemy.units.length; j++) {
+       if (
+         enemy.units[j] &&
+         player.projectiles[i] &&
+         isColliding(player.projectiles[i], enemy.units[j])
+       ) {
+         enemy.units[j].health -= player.projectiles[i].power;
+         player.projectiles.splice(i, 1);
+         i--;
+       }
+     }
 
-class Defender extends BaseUnit {
-  constructor(x, y) {
-    const width = board.cell.size - board.cell.gap * 2;
-    const height = board.cell.size - board.cell.gap * 2;
-    const type = board.units.player[player.selectedUnit];
-    super(type, x, y, width, height, 100, 0, 1, 167, 256);
-    this.shooting = false;
-    this.shootNow = false;
-    this.timer = 0;
-  }
+     if (
+       player.projectiles[i] &&
+       player.projectiles[i].x > canvas.width - board.cell.size
+     ) {
+       player.projectiles.splice(i, 1);
+       i--;
+     }
+   }
+ }
 
-  update() {
-    if (gameState.frame % 8 === 0) {
-      this.nextSpriteFrame();
-      if (this.frameX === 1) this.shootNow = true;
-    }
-    if (this.shooting && this.shootNow) {
-      player.projectiles.push(new Projectile(this.x + 70, this.y + 50));
-      this.shootNow = false;
-    }
-  }
+ /***********************************************************
+  *              D E F E N D E R S
+  */
 
-  draw() {
-    this.drawSprite();
-    this.drawHP("gold", "20px Arial", this.x + 15, this.y + 30);
-  }
-}
+ class Defender extends BaseUnit {
+   constructor(x, y) {
+     const width = board.cell.size - board.cell.gap * 2;
+     const height = board.cell.size - board.cell.gap * 2;
+     const sprite = playable.units.player[player.selectedUnit];
+     super(x, y, width, height, 100, sprite);
+     this.shooting = false;
+     this.shootNow = false;
+     this.timer = 0;
+   }
 
-function handleDefenders() {
-  for (let i = 0; i < player.units.length; i++) {
-    const unit = player.units[i];
-    unit.update();
-    unit.draw();
-    unit.shooting = enemy.positions.indexOf(unit.y) !== -1;
-    for (let j = 0; j < enemy.units.length; j++) {
-      const enemyUnit = enemy.units[j];
-      if (unit && isColliding(unit, enemyUnit)) {
-        unit.health -= 0.5;
-        enemyUnit.movement = 0;
-      }
-      if (unit && unit.health <= 0) {
-        player.units.splice(i, 1);
-        i--;
-        enemyUnit.movement = enemyUnit.speed;
-      }
-    }
-  }
-}
+   update() {
+     if (gameState.frame % 8 === 0) {
+       this.nextSpriteFrame();
+       if (this.frameX === 1) this.shootNow = true;
+     }
+     if (this.shooting && this.shootNow) {
+       player.projectiles.push(new Projectile(this.x + 70, this.y + 50));
+       this.shootNow = false;
+     }
+   }
 
-function chooseDefender() {
-  ctx.lineWidth = 1;
-  ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
-  let currentX = 20;
-  let currentY = 15;
-  board.units.player.forEach((uc, i) => {
-    if (isColliding(mouse, uc) && mouse.clicked) player.selectedUnit = i;
-    ctx.fillRect(uc.x, uc.y, uc.width, uc.height);
-    ctx.drawImage(uc.image, 0, 0, 167, 256, currentX, currentY, 50, 80);
-    const strokes = actionBar.unitStrokes;
-    ctx.strokeStyle =
-      player.selectedUnit === i ? strokes.selected : strokes.normal;
-    ctx.strokeRect(uc.x, uc.y, uc.width, uc.height);
-    currentX += 80;
-  });
-}
+   draw() {
+     this.drawSprite();
+     this.drawHP("gold", "20px Arial", this.x + 15, this.y + 30);
+   }
+ }
 
-/***********************************************************
- *              E N E M I E S
- */
+ function handleDefenders() {
+   for (let i = 0; i < player.units.length; i++) {
+     const unit = player.units[i];
+     unit.update();
+     unit.draw();
+     unit.shooting = enemy.positions.indexOf(unit.y) !== -1;
+     for (let j = 0; j < enemy.units.length; j++) {
+       const enemyUnit = enemy.units[j];
+       if (unit && isColliding(unit, enemyUnit)) {
+         unit.health -= 0.5;
+         enemyUnit.movement = 0;
+       }
+       if (unit && unit.health <= 0) {
+         player.units.splice(i, 1);
+         i--;
+         enemyUnit.movement = enemyUnit.speed;
+       }
+     }
+   }
+ }
 
-class Enemy extends BaseUnit {
-  constructor(yPos) {
-    const type = board.units.enemy[randomUpTo(board.units.enemy.length, true)];
-    const width = board.cell.size - board.cell.gap * 2;
-    const height = board.cell.size - board.cell.gap * 2;
-    super(type, canvas.width, yPos, width, height, 100, 0, 7, 290, 420);
-    this.speed = randomUpTo(0.8) + 0.4;
-    this.movement = this.speed;
-  }
+ function chooseDefender() {
+   ctx.lineWidth = 1;
+   ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+   let currentX = 7;
+   let currentY = 7;
+   const iconWidth = 70;
+   const iconHeight = 85;
+   const gapBetweenIcons = 3;
+   playable.units.player.forEach((pu, i) => {
+     ctx.fillRect(currentX, currentY, iconWidth, iconHeight);
+     const coords = {
+       x: currentX,
+       y: currentY,
+       width: pu.width,
+       height: pu.height,
+     };
+     if (isColliding(mouse, coords) && mouse.clicked) {
+       player.selectedUnit = i;
+     }
+     const strokes = actionBar.unitStrokes;
+     ctx.strokeStyle =
+       player.selectedUnit === i ? strokes.selected : strokes.normal;
+     ctx.strokeRect(currentX, currentY, iconWidth, iconHeight);
+     currentX += 10;
+     currentY += gapBetweenIcons;
+     ctx.drawImage(
+       pu.image,
+       0,
+       0,
+       pu.width,
+       pu.height,
+       currentX,
+       currentY,
+       50,
+       80
+     );
+     currentY -= gapBetweenIcons;
+     currentX += iconWidth + gapBetweenIcons;
+   });
+ }
 
-  update() {
-    this.x -= this.movement;
-    if (gameState.frame % 2 === 0) {
-      this.nextSpriteFrame();
-    }
-  }
+ /***********************************************************
+  *              E N E M I E S
+  */
 
-  draw() {
-    this.drawHP("black", "20px Arial", this.x + 15, this.y + 30);
-    this.drawSprite();
-  }
-}
+ class Enemy extends BaseUnit {
+   constructor(yPos) {
+     const sprite =
+       playable.units.enemy[randomUpTo(playable.units.enemy.length, true)];
+     const width = board.cell.size - board.cell.gap * 2;
+     const height = board.cell.size - board.cell.gap * 2;
+     super(canvas.width, yPos, width, height, 100, sprite);
+     this.speed = randomUpTo(0.8) + 0.4;
+     this.movement = this.speed;
+   }
+
+   update() {
+     this.x -= this.movement;
+     if (gameState.frame % 2 === 0) {
+       this.nextSpriteFrame();
+     }
+   }
+
+   draw() {
+     this.drawHP("black", "20px Arial", this.x + 15, this.y + 30);
+     this.drawSprite();
+   }
+ }
 
 function handleEnemies() {
   for (let i = 0; i < enemy.units.length; i++) {
